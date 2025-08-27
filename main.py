0ba2e139e2a856c4346998e799f531b6c854d01e
nikhil1w <rayrambadan70@gmail.com>
2025-08-27 17:36:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ bot = Client("bot",
              #bot_token= os.environ.get("BOT_TOKEN"),
              api_id= 23031620,
              api_hash= "31cb00c1cbe580394778b43105864bca")
-auth_users = [502980590]
+auth_users = [7445620075]
 #romeo  
 
-owner_id = 502980590
+owner_id = 7445620075
 # Extras 
 failed_links = []  # List to store failed links
 fail_cap =f"**➜ This file Contain Failed Downloads while Downloding \n You Can Retry them one more time **"
